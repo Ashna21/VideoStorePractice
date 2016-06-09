0556e626e0f82487a566bc47ca2de3339d68f3df
Ashna21 <aqd5316@psu.edu>
2016-06-09 12:50:06 -0400

    Add project files.

---
+++ VideoStore/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VideoStore.Startup))]
+namespace VideoStore
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
